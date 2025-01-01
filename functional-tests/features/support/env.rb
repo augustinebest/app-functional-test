980d604b537ab31223b5448fddee92a6e078dcfb
Augustine Igwe <Augustine.Igwe.Contractor@dvla.gov.uk>
2025-01-01 20:10:46 +0000

    required logger

--- functional-tests/features/support/env.rb
+++ functional-tests/features/support/env.rb
@@ -5,3 +5,8 @@ require 'dotenv/load'
 Capybara.default_driver = :selenium_chrome
 Capybara.app_host = ENV['BASE_URL'] || 'http://localhost:3000'
 Capybara.default_max_wait_time = 5
+
+require 'logger'
+
+LOG = Logger.new(STDOUT)
+LOG.level = Logger::INFO
