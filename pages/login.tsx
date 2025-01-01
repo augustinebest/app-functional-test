ed34f9f6e751e4c31b241546c25f94e80fbc2f2e
Augustine Igwe <Augustine.Igwe.Contractor@dvla.gov.uk>
2025-01-01 20:12:03 +0000

    improved dashboard page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,22 @@
+import { useRouter } from "next/router";
+
 export default function LoginPage() {
-  return <h1>Login Page </h1>;
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push("/dashboard");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" name="username" />
+
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" name="password" />
+
+      <button type="submit">Login</button>
+    </form>
+  );
 }
